1e5a3dfab7b5bd7acc1d0fcacad56a51d9108f04
ZahwaAulia <zahwa24ti@mahasiswa.pcr.ac.id>
2025-12-11 11:56:19 +0700

    p11

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\DasboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dashboard', [DasboardController::class, 'index'])->name('admin.dashboard');
+
