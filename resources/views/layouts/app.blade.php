bfabf2a553c6854e102bcdcf66d755aaa98845ee
ZahwaAulia <zahwa24ti@mahasiswa.pcr.ac.id>
2025-12-18 15:20:09 +0700

    j

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+
+    {{-- CSS --}}
+    <link href="{{ asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
+</head>
+<body>
+
+    {{-- ISI HALAMAN AKAN MUNCUL DI SINI --}}
+    @yield('content')
+
+    {{-- JS --}}
+    <script src="{{ asset('assets/js/argon-dashboard.js') }}"></script>
+</body>
+</html>
+ hv ggvctyfy bhb
